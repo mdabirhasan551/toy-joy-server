23abcbf07db38a8add83e483e041d961053ec305
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-25 13:29:47 +0600

    blogsData added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const data = require("./data/allToys.json");
+const blogsData = require("./data/blogs.json");
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send(data);
+});
+app.get("/blogs", (req, res) => {
+  res.send(blogsData);
+});
+app.listen(port, () => {
+  console.log("server is running at port: ", port);
+});
